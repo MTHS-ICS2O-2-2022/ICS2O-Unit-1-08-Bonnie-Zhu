b19a12cb7c1e5515144272dc8f7267cc56b9cb3d
Bonnie-Zhu <124389118+Bonnie-Zhu@users.noreply.github.com>
2023-03-01 18:23:41 +0000

    done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: Bonnie Zhu
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+function myButtonClicked() {
+  alert("The answer is..")
+}
