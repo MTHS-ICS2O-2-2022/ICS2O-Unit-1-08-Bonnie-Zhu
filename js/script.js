93ef399444bd0338b76053aed5f02909e969b1e4
Bonnie-Zhu <124389118+Bonnie-Zhu@users.noreply.github.com>
2023-05-24 16:30:13 +0000

    done

--- js/script.js
+++ js/script.js
@@ -4,6 +4,15 @@
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
 
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
+    scope: "/ICS2O-PWA-Test/",
+  })
+}
+
 function myButtonClicked() {
   alert("The answer is..")
 }
